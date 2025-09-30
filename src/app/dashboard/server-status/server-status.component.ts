61a333ef515c4bed6b5b932da44274c57e695291
hashimys <hashimys@gmail.com>
2025-09-30 11:57:44 +0300

    feat: refactor dashboard components for improved structure and styling, enhance server status logic, and update ticket handling

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject,  OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-    currentStatus = 'online';
+export class ServerStatusComponent implements OnInit{
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  destroyRef = inject(DestroyRef);
 
+  constructor() {}
+
+  ngOnInit() {
+    console.log('On Init');
+   const interval = setInterval(() => {
+      const rnd = Math.random();
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
+  }
+  ngAfterViewInit() {
+    console.log('After View Init');
+  }
 
 }
